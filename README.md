0c9b9bfbf1f852b50237e6bec638c61f7512d660
Namsulee <ynshero@gmail.com>
2017-02-12 18:53:43 +0900

    update pet feeder solution

---
+++ README.md
@@ -0,0 +1,15 @@
+Pet Feeder is a part of solution in Pet Management Solution.
+Pet management solution consists of 2 parts which are Pet feeder and Pet manager
+This repository is for Pet Feeder how to make up with EV3 and Arduino.
+You can get these codes and utilize it in your projects.
+
+## Arduino
+ : Arduino code sections, which has functions below.
+### Bluetooth
+ : It will be connected via bluetooth, we can send data like 'FEED' for feeding.
+### Switch
+ : Interrupt section for feeding, If we press switch on the Arduino, it will be sent in order to feed.
+### IR Receiver
+ : Infraed receiver will take signal from IR sender to feed.
+## EV3
+ : EV3 will be Pet feeder and distribute some food according to command of Arduino.
---
+++ arduino/PetFeederV1.ino
@@ -0,0 +1,160 @@
+#include <SoftwareSerial.h> // For Bluetooth, HP-06
+#include <Wire.h>
+#include <IRremote.h>
+
+#define LED_PIN 12
+#define SWITCH_PIN 2
+#define RECV_PIN  11
+#define SLAVE_ADDRESS 0x04
+#define DEBUG_ENABLE 1
+SoftwareSerial BTSerial(3, 4);
+IRrecv irrecv(RECV_PIN);
+boolean recording = true;
+decode_results results;
+
+typedef enum {
+  DS_NONE = 0,
+  DS_BLUETOOTH = 1,
+  DS_SWITCH = 2,
+  DS_IR = 3
+}DeviceStatus;
+
+boolean BT_STATUS = false;
+boolean SWITCH_STATUS = false;
+boolean IR_STATUS = false;
+boolean fFeeding = false;
+  
+void setup() {
+  // Serial For Debugging
+  Serial.begin(9600);
+  
+  // LED
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN, HIGH);
+
+  delay(2000);
+  // Bluetooth
+  BTSerial.begin(9600);  
+  
+  // Switch
+  pinMode(SWITCH_PIN, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(SWITCH_PIN), triggerSwitchEve, RISING);
+
+  // EV3 Connection I/F
+  Wire.begin(SLAVE_ADDRESS);
+  Wire.onRequest(sendData_I2C);
+  
+  // IR Receiver
+  irrecv.enableIRIn(); // Start the receiver
+
+  digitalWrite(LED_PIN, LOW);
+  Serial.println("Start Pet Feeder V1.0"); 
+}
+
+void setStatus(boolean value)
+{
+  fFeeding = value;
+}
+
+boolean getStatusFlag()
+{
+  return fFeeding;
+}
+
+boolean checkBTData() {
+  String myString="";
+  while(BTSerial.available())  //mySerial에 전송된 값이 있으면
+  {
+    char myChar = (char)BTSerial.read();  // convert data from int to char
+    myString+=myChar;   //Copy consecutive data
+    delay(5);           // Protect diconnection
+  }
+  if(!myString.equals(""))  // If value is not empty
+  {
+    Serial.println("input value: "+myString); // For debugging
+    if(myString.equals("FEED")) {
+      Serial.println("FEEDING"); 
+      if (!getStatusFlag()) {
+        setStatus(true);
+        digitalWrite(LED_PIN, HIGH);
+        BT_STATUS = true;
+      }
+      myString="";  // Initialize
+    }
+  }
+  return BT_STATUS;
+}
+
+void triggerSwitchEve()
+{
+  Serial.println("Button Pressed");
+  if (!getStatusFlag()) {
+    setStatus(true);
+    digitalWrite(LED_PIN, HIGH);
+    SWITCH_STATUS = true;
+  }
+}
+
+boolean checkIRData() {
+  if (irrecv.decode(&results)) {  
+    if(!getStatusFlag()) {
+      setStatus(true);
+      digitalWrite(LED_PIN, HIGH);
+#if DEBUG_ENABLE
+      Serial.println(results.value, HEX); 
+#endif
+      irrecv.resume();
+      IR_STATUS = true;
+      return IR_STATUS;
+    } 
+  }
+  return false;
+}
+
+void clearAllDevStatus() {
+  if(BT_STATUS) BT_STATUS = false;
+  if(SWITCH_STATUS) SWITCH_STATUS = false;
+  if(IR_STATUS) IR_STATUS = false;
+}
+
+// callback for sending data
+void sendData_I2C()
+{
+#if DEBUG_ENABLE
+  Serial.println("Calling from EV3 which request data");
+#endif
+  if(getStatusFlag() == true) {
+     Wire.write("1");
+     setStatus(false);
+     clearAllDevStatus();
+     digitalWrite(LED_PIN, LOW);
+  }
+}
+
+DeviceStatus checkInputMethod() {
+  DeviceStatus enStatus = DS_NONE;
+  
+  if (checkBTData()) {
+   enStatus = DS_BLUETOOTH;  
+  }
+  else if (SWITCH_STATUS) {
+    enStatus = DS_SWITCH;
+  }
+  else if (checkIRData()) {
+    enStatus = DS_IR;
+  }
+  else {
+    enStatus =  DS_NONE;
+  }
+  return enStatus;
+}
+
+void loop() {
+  DeviceStatus methodStatus = DS_NONE;
+  // check which device makes trigger for feeding
+  methodStatus = checkInputMethod();
+#if DEBUG_ENABLE
+  //Serial.print("MethodStatus = ");
+  //Serial.println(methodStatus);
+#endif
+}
